3945059668c7e4d9f8560b3355eb95a0c6b5b7a3
Sheeb <thingisss89@gmail.com>
2024-05-11 23:44:47 -0700

    cambios.

--- app.js
+++ app.js
@@ -75,7 +75,6 @@ function fetchAnimeData(searchTerm) {
         .catch(error => console.error('Error:', error));
 }
 
-
 function viewAnimeDetails(animeId) {
     var query = `
     query ($id: Int) {
@@ -137,12 +136,74 @@ function viewAnimeDetails(animeId) {
             `;
             document.getElementById("animeModalBody").innerHTML = modalBody;
 
-            setTimeout(function() {
+            setTimeout(function () {
                 var myModal = new bootstrap.Modal(document.getElementById('animeModal'));
                 myModal.show();
             }, 100);
         })
         .catch(error => console.error('Error:', error));
 }
-  
+
+// Función para obtener las recomendaciones de anime de AniList
+// Función para obtener las recomendaciones de anime de AniList
+async function getAnimeRecommendations() {
+    const query = `
+        query {
+            recommendations(genre: "Action", limit: 10) {
+                id
+                title {
+                    romaji
+                }
+                coverImage {
+                    large
+                }
+                description
+            }
+        }
+    `;
+
+    const url = 'https://graphql.anilist.co';
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            query: query
+        })
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const data = await response.json();
+        return data.data.recommendations;
+    } catch (error) {
+        console.log('Error: ', error);
+        return []; // Devolver un array vacío en caso de error
+    }
+}
+
+getAnimeRecommendations().then(recommendations => {
+    // Limitar las recomendaciones a 4
+    const recommendationsLimit = 4;
+    recommendations.slice(0, recommendationsLimit).forEach(recommendation => {
+        // Crear un card de anime
+        const card = `
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="${recommendation.image}" class="card-img-top" alt="${recommendation.title}" height="200" />
+                    <div class="card-body">
+                        <h5 class="card-title">${recommendation.title}</h5>
+                        <p class="card-text">${recommendation.description}</p>
+                    </div>
+                </div>
+            </div>
+            `;
+        // Agregar el card al contenido de la página
+        document.querySelector('.row.mb-3').innerHTML += card;
+    });
+});
+
+
 document.getElementById("searchForm").addEventListener("submit", searchAnime);
